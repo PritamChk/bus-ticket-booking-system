37549fcba2e7bb5dede0ddef7ab45be6a27e3988
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-06-21 13:15:14 +0530

    booking details component initiated

---
+++ src/components/BookingDetailBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="border-2 text-4xl bg-zinc-200 rounded-md mx-2 my-5">
+    Booking Details
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookingDetailBox",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="h-screen grid grid-flow-col grid-cols-2">
     <BusVue class="col-span-1" />
+    <BookingDetailBoxVue />
   </div>
 </template>
 
 <script>
 import BusVue from "../components/Bus.vue";
+import BookingDetailBoxVue from "../components/BookingDetailBox.vue";
 export default {
   components: {
     BusVue,
+    BookingDetailBoxVue,
   },
   data() {
     return {
